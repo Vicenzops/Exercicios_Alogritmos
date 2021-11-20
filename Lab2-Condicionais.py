22fe74a1632cc136ce358ce91d66c9811a001539
Vicenzops <77994866+Vicenzops@users.noreply.github.com>
2021-11-19 23:07:59 -0300

    Programa usando Estruturas Condicionais e Operadores Relacionais

---
+++ Lab2-Condicionais.py
@@ -0,0 +1,12 @@
+idade = input('Digite a faixa etária: ')
+
+if idade == "Bebê":
+    print('menor que 2 anos')
+if idade == "Criança":
+    print('de 3 a 10 anos')
+if idade == "Adolescente":
+    print('de 11 a 17 anos')
+if idade == "Adulto":
+    print('de 18 a 64 anos')
+if idade == "Idoso":
+    print('maior que 65 anos')
